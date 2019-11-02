dbbc878b35f0a38be91f657df056400c6122840a
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2019-11-02 10:54:04 -0300

    Script da tabela alunos #3

---
+++ alunos.sql
@@ -0,0 +1,17 @@
+--
+-- Banco de dados: `escola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `matricula` int(10) NOT NULL,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
