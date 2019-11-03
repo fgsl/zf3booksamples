83121fed119ea10f044da8b41d69a555628520e0
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2019-11-03 15:57:05 -0300

    Update alunos.sql

--- alunos.sql
+++ alunos.sql
@@ -9,7 +9,7 @@
 --
 
 CREATE TABLE `alunos` (
-  `matricula` int(10) NOT NULL,
+  `matricula` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`matricula`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
