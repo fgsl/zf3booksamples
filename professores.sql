0797397545b8b07758fe2508fc35b3fac047851b
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2019-11-03 15:57:49 -0300

    Update professores.sql

--- professores.sql
+++ professores.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `professores` (
-  `codigo` int(10) NOT NULL,
+  `codigo` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
